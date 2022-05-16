4ec69a178450e4b53d40548d7599f99968b98c86
severine legon <severine@multiply.ai>
2022-05-16 21:46:42 +0100

    feat: create bullet point component

---
+++ src/components/BulletPoint.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { colors, spacing } from "../styles";
+
+type BulletPointProps = {
+  //   sentences: readonly [string, string?, string?, string?, string?];
+  sentences: string[];
+};
+
+export const BulletPoint = ({ sentences }: BulletPointProps) => {
+  return (
+    <View>
+      {sentences.map((sentence, index) => {
+        return (
+          <View key={index} style={styles.container}>
+            <View style={styles.circle}>
+              <Text style={styles.bulletNumber}>{index + 1}</Text>
+            </View>
+            <Text style={styles.bulletText}>{sentence}</Text>
+          </View>
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    marginBottom: spacing.base,
+  },
+  circle: {
+    height: 30,
+    width: 30,
+    borderRadius: 30,
+    backgroundColor: colors.grey,
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: spacing.baseSmall,
+  },
+  bulletText: {
+    color: colors.brown,
+    flexShrink: 1,
+    marginTop: spacing.tiniest,
+    fontSize: 18,
+  },
+  bulletNumber: {
+    color: colors.white,
+  },
+});
